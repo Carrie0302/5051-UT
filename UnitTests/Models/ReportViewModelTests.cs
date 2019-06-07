b6a05f152da9495339f2527cb315946a21696a59
Carrie <clipsong@gmail.com>
2019-06-07 10:56:29 -0700

    Added ReportViewModel set test

--- UnitTests/Models/ReportViewModelTests.cs
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using HW1c.Models;
+using System.Collections.Generic;
 
 namespace UnitTests.Models
 {
@@ -20,5 +21,20 @@ namespace UnitTests.Models
             Assert.IsNotNull(result);
 
         }
+
+
+        [TestMethod]
+        public void ReportViewModel_Get_Default_Should_Pass()
+        {
+            //Arrange
+            var myTest = new ReportViewModel();
+
+            //Action
+            var result = myTest.LogViewModel;
+
+            //Assert
+            Assert.IsNotNull(result);
+        }
+
     }
 }
