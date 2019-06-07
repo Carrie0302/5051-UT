27835afe4f2738b90d06df277db17eb3fb0fb92d
Carrie <clipsong@gmail.com>
2019-06-07 10:41:51 -0700

    Added ReportViewModelTests.cs to the project

---
+++ UnitTests/Models/ReportViewModelTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HW1c.Models;
+
+namespace UnitTests.Models
+{
+    [TestClass]
+    public class ReportViewModelTests
+    {
+        [TestMethod]
+        public void ReportViewModel_Instantiate_Default_Should_Pass()
+        {
+
+        }
+    }
+}
